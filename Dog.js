57b00baefee49a3b9b5fa7e04881d66640eb4f90
zayaanw <zayaanw@gmail.com>
2023-05-18 15:43:16 +0100

    Completed

--- Dog.js
+++ Dog.js
@@ -3,7 +3,7 @@ class Dogs {
     Object.assign(this, data);
   }
   getDogHtml() {
-    let { name, avatar, age, bio, hasbeenSwiped, hasbeenLiked } = this;
+    let { name, age, bio } = this;
     return ` <h2 class="dog_name">${name}, ${age}</h2>
     <p class="dog_bio">${bio}</p>`;
   }
--- index.js
+++ index.js
@@ -23,27 +23,49 @@ function render() {
   document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
   document.querySelector(".main_content").style.backgroundImage =
     dogProfile.dogAvatar();
-  if (dogProfile.hasbeenSwiped && dogProfile.hasbeenLiked) {
-    document.querySelector(
-      ".badge_img"
-    ).innerHTML = `<img src="assets/images/badge-like.png" alt="" class="badge" />`;
-    nextProfile();
-  } else if (dogProfile.hasbeenSwiped) {
-    document.querySelector(
-      ".badge_img"
-    ).innerHTML = `<img src="assets/images/badge-nope.png" alt="" class="badge" />`;
-    nextProfile();
+  if (dogs.length > 0) {
+    if (dogProfile.hasbeenSwiped && dogProfile.hasbeenLiked) {
+      document.querySelector(
+        ".badge_img"
+      ).innerHTML = `<img src="assets/images/badge-like.png" alt="" class="badge" />`;
+      nextProfile();
+    } else if (dogProfile.hasbeenSwiped) {
+      document.querySelector(
+        ".badge_img"
+      ).innerHTML = `<img src="assets/images/badge-nope.png" alt="" class="badge" />`;
+      nextProfile();
+    }
+  } else {
+    noMatches();
   }
-}
 
-function nextProfile() {
-  setTimeout(() => {
-    document.querySelector(".badge_img").innerHTML = ``;
-    dogProfile = getnewDog();
-    document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
-    document.querySelector(".main_content").style.backgroundImage =
-      dogProfile.dogAvatar();
-  }, 1500);
+  function nextProfile() {
+    setTimeout(() => {
+      document.querySelector(".badge_img").innerHTML = ``;
+      dogProfile = getnewDog();
+      document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
+      document.querySelector(".main_content").style.backgroundImage =
+        dogProfile.dogAvatar();
+    }, 1500);
+  }
+
+  function noMatches() {
+    if (dogProfile.hasbeenSwiped && dogProfile.hasbeenLiked) {
+      document.querySelector(
+        ".badge_img"
+      ).innerHTML = `<img src="assets/images/badge-like.png" alt="" class="badge" />`;
+    } else if (dogProfile.hasbeenSwiped) {
+      document.querySelector(
+        ".badge_img"
+      ).innerHTML = `<img src="assets/images/badge-nope.png" alt="" class="badge" />`;
+    }
+    setTimeout(() => {
+      document.querySelector(".main_content").style.backgroundImage = "";
+      document.querySelector(
+        ".main_content"
+      ).innerHTML = `<h2 class="no_matches"> No more matches :( </h2>`;
+    }, 1500);
+  }
 }
 
 render();
