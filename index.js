2e18648af116955196e27bbfd59d361f5b48d2d7
zayaanw <zayaanw@gmail.com>
2023-05-18 14:50:46 +0100

    working app outside of no more matches

--- index.js
+++ index.js
@@ -8,17 +8,50 @@ function getnewDog() {
 
 let dogProfile = getnewDog();
 
-console.log(dogProfile);
-
 document.querySelector("#icon-cross").addEventListener("click", () => {
   if (dogProfile.hasbeenSwiped) {
-    dogProfile = getnewDog();
-    document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
-    document.querySelector(".main_content").style.backgroundImage =
-      dogProfile.dogAvatar();
+    document.querySelector(
+      ".badge_img"
+    ).innerHTML = `<img src="assets/images/badge-nope.png" alt="" class="badge" />`;
+
+    setTimeout(() => {
+      document.querySelector(".badge_img").innerHTML = ``;
+      dogProfile = getnewDog();
+      document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
+      document.querySelector(".main_content").style.backgroundImage =
+        dogProfile.dogAvatar();
+    }, 1500);
   } else {
     document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
     document.querySelector(".main_content").style.backgroundImage =
       dogProfile.dogAvatar();
   }
 });
+
+document.querySelector("#icon-heart").addEventListener("click", () => {
+  if (dogProfile.hasbeenSwiped) {
+    document.querySelector(
+      ".badge_img"
+    ).innerHTML = `<img src="assets/images/badge-like.png" alt="" class="badge" />`;
+
+    setTimeout(() => {
+      document.querySelector(".badge_img").innerHTML = ``;
+      dogProfile = getnewDog();
+      document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
+      document.querySelector(".main_content").style.backgroundImage =
+        dogProfile.dogAvatar();
+    }, 1500);
+  } else {
+    document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
+    document.querySelector(".main_content").style.backgroundImage =
+      dogProfile.dogAvatar();
+  }
+});
+
+function render() {
+  document.querySelector(".main_inner").innerHTML = dogProfile.getDogHtml();
+  document.querySelector(".main_content").style.backgroundImage =
+    dogProfile.dogAvatar();
+}
+
+render();
